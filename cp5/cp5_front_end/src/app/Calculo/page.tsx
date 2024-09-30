184e04fcd722f121827045fbf50bb40e49900c5b
Rafa130206 <rafabompadre@gmail.com>
2024-09-30 20:45:14 -0300

    ajustes.

--- cp5/cp5_front_end/src/app/Calculo/page.tsx
+++ cp5/cp5_front_end/src/app/Calculo/page.tsx
@@ -10,8 +10,8 @@ export default function Calculo() {
             <p>
                 No entanto, Velikovsky faz algumas inferências qualitativas sobre a força gravitacional entre planetas e as possíveis consequências dessas interações. Para entender o tipo de cálculo que ele teria proposto, é possível reconstruir uma situação em que a passagem de Vênus, ou de Marte, perto da Terra causasse perturbações significativas, como mudanças na rotação terrestre ou grandes marés.
             </p>
-            <Calculo1 src="/assets/calculo1" alt='Lei da Gravitação Universal' width={187} height={71}/>
-            <Calculo2 src="/assets/calculo2" alt='Força Gravitacional Hipotética' width={462} height={77}/>
+            <Calculo1 src="/assets/calculo1.png" alt='Lei da Gravitação Universal' width={187} height={71}/>
+            <Calculo2 src="/assets/calculo2.png" alt='Força Gravitacional Hipotética' width={462} height={77}/>
             <p>
                 Essa força seria incrivelmente massiva, e se um evento desse tipo tivesse realmente ocorrido, causaria perturbações devastadoras na Terra, como mudanças na órbita, marés gigantescas, e outros desastres naturais, como sugerido por Velikovsky em seu livro.
             </p>
--- cp5/cp5_front_end/src/app/Catastrofe/page.tsx
+++ cp5/cp5_front_end/src/app/Catastrofe/page.tsx
@@ -6,7 +6,7 @@ export default function catastrofe() {
             <p>
                 Velikovsky também propôs que essas interações planetárias não foram únicas, mas ocorreram várias vezes ao longo da história. Após a passagem de Vênus, ele sugere que Marte também passou perto da Terra em cerca de 700 a.C., causando outros desastres naturais e eventos que foram registrados nas mitologias e tradições de culturas antigas.
             </p>
-            <Imagem src="/assets/catastrofe.jpg" alt='Catástrofe' width={1120} height={630}/>
+            <Imagem src="/assets/catastrofe.jpg" alt='Catástrofe' width={1120} height={630} className='catastrofe'/>
         </div>
     )
 }
